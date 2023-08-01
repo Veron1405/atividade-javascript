94750c0ee90fc0933db700f897e7cf10f2948d65
Veron1405 <133662401+Veron1405@users.noreply.github.com>
2023-08-01 17:01:47 -0300

    Add files via upload

---
+++ Atividade Javascript/script.js
@@ -0,0 +1,9 @@
+function calcularArea()
+ {
+    let lado = prompt("Digite o valor do lado do quadrado:");
+    lado = parseFloat(lado);
+
+      let area = lado * lado;
+      alert("A área do quadrado é: " + area);
+    }
+  
