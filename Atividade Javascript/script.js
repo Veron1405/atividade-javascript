82d6f970285a812589bdbd0dbbea0b56b7a34955
Veron1405 <leonardo.verondias1405@gmail.com>
2023-08-02 22:35:16 -0300

    www

--- Atividade Javascript/script.js
+++ Atividade Javascript/script.js
@@ -6,4 +6,59 @@ function calcularArea()
       let area = lado * lado;
       alert("A área do quadrado é: " + area);
     }
+
+function calcularAreaTriangulo() {
+
+      let base = parseFloat(prompt("Digite o valor da base do triângulo:"));
+      let altura = parseFloat(prompt("Digite o valor da altura do triângulo:"));
+     
+      var area = (base * altura) / 2;
   
+      alert("A área do triângulo é: " + area.toFixed(2));
+  }
+
+  function calcularAreaRetangulo() {
+
+    let base = parseFloat(prompt("Digite o valor da base do retângulo:"));
+    let altura = parseFloat(prompt("Digite o valor da altura do retângulo:"));
+
+    let area = base * altura;
+
+    alert("A área do retângulo é: " + area.toFixed(2));
+}
+
+function calcularAreaTrapezio() {
+
+  let baseMaior = parseFloat(prompt("Digite o valor da base maior do trapézio:"));
+  let baseMenor = parseFloat(prompt("Digite o valor da base menor do trapézio:"));
+  let altura = parseFloat(prompt("Digite o valor da altura do trapézio:"));
+
+  let area = ((baseMaior + baseMenor) * altura) / 2;
+
+  alert("A área do trapézio é: " + area.toFixed(2));
+}
+
+function calcularAreaCirculo() {
+
+  let raio = parseFloat(prompt("Digite o valor do raio do círculo:"));
+
+  let area = Math.PI * raio * raio;
+
+  alert("A área do círculo é: " + area.toFixed(2));
+}
+
+function exibirInformacao() {
+
+  let informacao = prompt("Digite uma informação:");
+
+  if (informacao === null || informacao.trim() === "") {
+      alert("Você precisa digitar uma informação válida!");
+  } else {
+
+      let elementoParagrafo = document.createElement("p");
+
+      elementoParagrafo.textContent = informacao;
+
+      document.body.appendChild(elementoParagrafo);
+  }
+}
